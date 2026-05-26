f5c86a15f0ebb82d0a7281a1bf39579f52a6b400
tukhv <tukhvatov.islam@mail.ru>
2026-05-26 12:12:35 +0300

    add example init and 'q' - exit

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ def math_trainer():
     total_questions = 0
     response_times = []
 
+    while True:
+        # Генерация чисел для умножения (от 1 до 10)
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        correct_answer = num1 * num2
+
+
+
+
+        # Вывод примера на экран игрока
+        player_input = input(f"{num1} × {num2} = ")
+
+        # Если игрок нажал q - выходим из программы.
+        if player_input.lower() == 'q':
+            break
+
+
+
