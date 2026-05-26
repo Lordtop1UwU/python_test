fe300bf11fc0cc4a3c3bdc6d8d22678d1947b669
tukhv <tukhvatov.islam@mail.ru>
2026-05-26 12:07:44 +0300

    library add and create func

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello world")
-x=5
-print(x)
+import random
+import time
+
+def math_trainer():
+    print("Добро пожаловать в тренажёр таблицы умножения!")
+    print("Для выхода введите 'q'")
+    print("-" * 40)
+
+    # Инициализация статистики
+    correct_answers = 0
+    total_questions = 0
+    response_times = []
+
